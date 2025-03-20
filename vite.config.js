ea2b25672b1844819fe05ba2d1af552cb3634657
joel <joelphilip100@gmail.com>
2025-03-20 18:21:14 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig, loadEnv } from "vite";
 
-export default defineConfig({
-  base: "/chai-code-assignment1/",
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: "/chai-code-assignment1/",
+    define: {
+      "import.meta.env": JSON.stringify(env),
+    },
+  };
 });
